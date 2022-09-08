1babf4f09cd1b23c0132a8b7ed6485cdc920cfe4
Sam <89226291+Sam-or-something@users.noreply.github.com>
2022-09-08 08:57:01 -0300

    proyector collider

---
+++ Assets/Proyector.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proyector : MonoBehaviour
+{
+    
+    private void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("proyectando");
+    }
+}
